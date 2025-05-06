3f95f32980e4e2326cae1ab46b3acfe8593999c3
Bill <bill@thewallisfamily.co.uk>
2025-05-06 11:20:45 +0100

    feat: add basic implementation

--- mash/main.py
+++ mash/main.py
@@ -2,12 +2,38 @@
 Entry point into the package.
 """
 
+import pathlib
+import random
+import tomllib
+from typing import Any
+
+HERE = pathlib.Path(__file__).parent
+
+
+def _print_results(results: dict[str:Any]):
+    """
+    Print the results of the game.
+    """
+
+    cat_len = max(len(category) for category in results.keys())
+    for category, option in results.items():
+        print(f"{category.rjust(cat_len)}: {option}")
+
 
 def main() -> None:
     """
     Entry point into the module.
     """
 
+    category_options = tomllib.loads(
+        (HERE / "categories.toml").read_text(encoding="utf-8")
+    )
+    results = {
+        category: random.choice(options)  # noqa: S311
+        for category, options in category_options.items()
+    }
+    _print_results(results)
+
 
 if __name__ == "__main__":
-    main()  # pragma: no cover
+    main()
