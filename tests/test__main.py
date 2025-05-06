e6bc9a9600242f18ab1af564b3543c1f63b3d2e1
Bill <bill@thewallisfamily.co.uk>
2025-05-06 11:20:45 +0100

    feat: add basic implementation

--- tests/test__main.py
+++ tests/test__main.py
@@ -1,12 +1,26 @@
-"""
-Unit tests for the ``mash.main`` module.
-"""
+import random
+import textwrap
 
 from mash import main
 
 
-def test__main():
+def test__results_are_printed_as_expected(capsys):
     """
-    Test the ``main`` function.
+    Test the results are printed as expected.
+
+    Note that this is a crappy test. It will be improved later.
     """
+
+    random.seed(0)
+
     main.main()
+    captured = capsys.readouterr()
+
+    assert captured.out == textwrap.dedent(
+        """\
+               attire: Casual Clothes
+                foods: Completely new food
+               drinks: Soft Only
+        entertainment: Home Games (video or board)
+        """
+    )
